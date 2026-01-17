f41194a82ac82c0ef6a76bc6d48b8afd9749a786
web-tiki <contact@web-tiki.com>
2026-01-17 10:10:58 +0100

    remove content from reponsiveimages.php

--- responsiveimages/responsiveimages.php
+++ responsiveimages/responsiveimages.php
@@ -10,32 +10,3 @@ declare(strict_types=1);
  */
 
 defined('_JEXEC') or die;
-
-use Joomla\CMS\Plugin\CMSPlugin;
-
-
-class PlgSystemResponsiveImages extends CMSPlugin
-{
-    /*
-    public function onAfterInitialise()
-    {
-        if ($this->app->isClient('administrator')) {
-            if ($this->app->input->getCmd('task') === 'plugin.purgeResponsiveCache') {
-                $this->purgeCache();
-            }
-        }
-    }
-
-    private function purgeCache()
-    {
-        $path = JPATH_ROOT . '/images/responsive';
-        if (is_dir($path)) {
-            Folder::delete($path);
-            $this->app->enqueueMessage('✅ Responsive image cache purged successfully.');
-        } else {
-            $this->app->enqueueMessage('ℹ️ No responsive cache directory found.');
-        }
-        $this->app->redirect('index.php?option=com_plugins&view=plugins&filter[folder]=system');
-    }
-    */
-}
